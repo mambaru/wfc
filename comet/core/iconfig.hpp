b9ef785d122bfc3a6972ae588a936576e1ff1ba9
Vladimir Migashko <migashko@gmail.com>
2013-11-22 20:26:54 +0400

    upd

--- comet/core/iconfig.hpp
+++ comet/core/iconfig.hpp
@@ -11,7 +11,7 @@ struct iconfig
   virtual bool parse_config(const std::string& path) = 0;
   virtual void configure(const std::string& path) = 0;
   virtual std::string get_config(const std::string& name) = 0;
-  virtual std::string generate(const std::string& type) = 0;
+  virtual std::string generate(const std::string& type, const std::string& path) = 0;
 };
 
 }}
