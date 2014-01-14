cd990d2c5c5ac4dcfee4f7c92f671e6b81b697f7
Vladimir Migashko <migashko@gmail.com>
2014-01-15 00:56:19 +0400

    upd

--- wfc/inet/srv/connection_manager.hpp
+++ wfc/inet/srv/connection_manager.hpp
@@ -13,15 +13,32 @@ class connection_manager
 {
   typedef std::mutex mutex_type;
   typedef std::shared_ptr<iconnection> connection_ptr;
+  //virtual boost::asio::ip::address remote_address() = 0;
+  //virtual unsigned short remote_port() = 0;
+  typedef std::pair<boost::asio::ip::address, unsigned short> endpoint_type;
+  
+  typedef std::map<endpoint_type,  connection_ptr> by_endpoint_map;
+  typedef std::map<time_t,  connection_ptr> by_activity_map;
+  typedef std::set< connection_ptr > connection_set;
 public:
-  void insert(connection_ptr conn)
+  bool insert(connection_ptr conn)
   {
+    endpoint_type ep( conn->remote_address(), conn->remote_port());
     std::lock_guard<mutex_type> lk(_mutex);
+    
+    auto ep_itr = _by_endpoint.find( ep );
+    if (ep_itr != _by_endpoint.end() )
+      return false; // TODO:
+
+    _by_activity.insert( std::make_pair(time(0),  conn) );
+    _by_endpoint.insert( std::make_pair(ep,  conn) );
     _connections.insert(conn);
+    return true;
   }
 
   void erase(connection_ptr conn)
   {
+#error
     std::lock_guard<mutex_type> lk(_mutex);
     _connections.erase(conn);
   }
@@ -35,8 +52,10 @@ public:
   }
   
 private:
-  std::set< connection_ptr > _connections;
-  std::mutex _mutex;
+  connection_set  _connections;
+  by_endpoint_map _by_endpoint;
+  by_activity_map _by_activity;
+  std::mutex     _mutex;
 };
 
 }}
