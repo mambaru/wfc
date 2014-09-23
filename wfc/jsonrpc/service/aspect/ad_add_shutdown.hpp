31aa080da9bc28263ba569dfb01f708f4dbfa792
Vladimir Migashko <migashko@gmail.com>
2014-09-23 20:08:50 +0400

    upd

--- wfc/jsonrpc/service/aspect/ad_add_shutdown.hpp
+++ wfc/jsonrpc/service/aspect/ad_add_shutdown.hpp
@@ -2,7 +2,6 @@
 
 #include <wfc/jsonrpc/service/aspect/tags.hpp>
 
-
 namespace wfc{ namespace jsonrpc{
   
 struct ad_add_shutdown
@@ -13,18 +12,12 @@ struct ad_add_shutdown
     typedef typename T::io_id_t io_id_t;
     add_shutdown([&t]( io_id_t io_id )
     {
-      //t.post([&t, io_id]()
+      if ( auto handler = t.registry().erase_io(io_id) )
       {
-        if ( auto handler = t.registry().erase_io(io_id) )
-        {
-          handler->stop(io_id);
-        }
-      }/*)*/;
+        handler->stop(io_id);
+      }
     });
   }
 };
-
   
 }} // wfc
-
-
