11c50aa8a0f5856ee25c341a1a47511e889471af
Vladimir Migashko <migashko@gmail.com>
2014-10-28 20:39:02 +0300

    provider upd

---
+++ wfc/provider/provider_config_json.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <wfc/provider/provider_config.hpp>
+
+namespace wfc{ namespace provider{ 
+  
+struct provider_config_json
+{
+  JSON_NAME(enabled)
+  JSON_NAME(sequence_mode)
+  JSON_NAME(queue_limit)
+  JSON_NAME(queue_warning)
+  JSON_NAME(timeout_ms)
+  
+  typedef ::wfc::json::object<
+    provider_config,
+    fas::type_list_n<
+      ::wfc::json::member<n_enabled,       provider_config, bool,   &provider_config::enabled>,
+      ::wfc::json::member<n_sequence_mode, provider_config, bool,   &provider_config::sequence_mode>,
+      ::wfc::json::member<n_queue_limit,   provider_config, size_t, &provider_config::queue_limit>,
+      ::wfc::json::member<n_queue_warning, provider_config, size_t, &provider_config::queue_warning>,
+      ::wfc::json::member<n_timeout_ms,    provider_config, time_t, &provider_config::wait_timeout_ms>
+    >::type
+  > type;
+  
+  typedef type::target target;
+  typedef type::member_list member_list;
+  typedef type::serializer serializer;
+};
+      
+
+}}
+
