990487e4dda8371555d0bd2c5a5e1072185a7cdf
Vladimir Migashko <migashko@gmail.com>
2014-11-04 23:17:09 +0300

    wfc upd

--- wfc/jsonrpc/method/method.hpp
+++ wfc/jsonrpc/method/method.hpp
@@ -9,8 +9,9 @@ namespace wfc{ namespace jsonrpc{
 
 template< typename... Args>
 struct method
-  : fas::type_list< basic_method< fas::aspect<Args...> >, 
-    fas::type_list< fas::group<_method_, typename basic_method< fas::aspect<Args...> >::tag>
+  : fas::type_list< 
+      basic_method< fas::aspect<Args...> >, fas::type_list< 
+      fas::group<_method_, typename basic_method< fas::aspect<Args...> >::tag>
     > > 
 {};
 
