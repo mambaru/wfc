4e22926a982329ec59027e3b17965af681e21c7a
Vladimir Migashko <migashko@gmail.com>
2015-05-27 02:41:21 +0300

    basic_io

--- wfc/core/iconfig.hpp
+++ wfc/core/iconfig.hpp
@@ -8,6 +8,7 @@
 
 #include <wfc/iinterface.hpp>
 #include <string>
+#include <map>
 
 namespace wfc{
 
@@ -15,9 +16,12 @@ struct iconfig: iinterface
 {
   virtual ~iconfig() {}
   virtual void reload_and_reconfigure() = 0;
+  // TODO: не из файла, передавать считанный json
   virtual void load_and_parse(std::string path) = 0;
   virtual std::string get_config(std::string name) = 0;
-  virtual std::string generate_and_write(std::string type, std::string path) = 0;
+  //virtual std::string generate_and_write(std::string type, std::string path) = 0;
+  typedef std::map<std::string, std::string> generate_options;
+  virtual bool generate_config( const generate_options& go, std::string& result) = 0;
 };
 
 }
