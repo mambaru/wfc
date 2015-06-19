35fd16408be5f314efd2102d89db5fc4a7e3e80c
Vladimir Migashko <migashko@gmail.com>
2015-06-19 16:20:46 +0300

    +mutex

---
+++ wfc/mutex.hpp
@@ -0,0 +1,15 @@
+//
+// Author: Vladimir Migashko <migashko@gmail.com>, (C) 2013-2015
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+
+#pragma once
+
+#include <iow/mutex.hpp>
+
+namespace wfc{ 
+
+using namespace iow;
+
+}
