bf1b28bbc561933a519bc92e6f0d8075ed622906
Vladimir Migashko <migashko@gmail.com>
2015-08-14 20:43:16 +0300

    upd

--- wfc/jsonrpc/ijsonrpc.hpp
+++ wfc/jsonrpc/ijsonrpc.hpp
@@ -1,6 +1,7 @@
-#pragma once 
+#pragma once
 
 #include <iow/jsonrpc/incoming/incoming_holder.hpp>
+#include <iow/jsonrpc/outgoing/outgoing_holder.hpp>
 #include <wfc/iinterface.hpp>
 
 namespace wfc{ namespace jsonrpc{
@@ -9,8 +10,11 @@ struct ijsonrpc
   : iinterface
 {
   typedef ::iow::jsonrpc::incoming_holder incoming_holder;
+  typedef ::iow::jsonrpc::outgoing_holder outgoing_holder;
+
   virtual ~ijsonrpc(){}
   virtual void perform_incoming(incoming_holder, io_id_t, outgoing_handler_t handler) = 0;
+  virtual void perform_outgoing(outgoing_holder, io_id_t) = 0;
 };
 
 }}
