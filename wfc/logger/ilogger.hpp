ae84de4454d139e13fdf1ade768a939c8d627210
Vladimir Migashko <migashko@gmail.com>
2015-09-11 02:53:14 +0300

    *ilogger::write

--- wfc/logger/ilogger.hpp
+++ wfc/logger/ilogger.hpp
@@ -10,7 +10,7 @@ struct ilogger: iinterface
 {
   virtual ~ilogger(){}
   //virtual void initialize( const std::string& name, std::stringstream& str ) = 0;
-  virtual void write(const std::string& name, const std::string& ident, const std::string& str) = 0;
+  virtual void write(const std::string& name, const std::string& ident, std::string str) = 0;
 };
 
 }
