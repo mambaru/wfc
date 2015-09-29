d23bb885c9ca8b2e40a313cbbd9f8037d9ad9e6c
Vladimir Migashko <migashko@gmail.com>
2015-09-30 00:10:15 +0300

    +WFC_LOG_...

--- wfc/logger/ilogger.hpp
+++ wfc/logger/ilogger.hpp
@@ -9,7 +9,6 @@ namespace wfc{
 struct ilogger: iinterface
 {
   virtual ~ilogger(){}
-  //virtual void initialize( const std::string& name, std::stringstream& str ) = 0;
   virtual void write(const std::string& name, const std::string& ident, std::string str) = 0;
 };
 
