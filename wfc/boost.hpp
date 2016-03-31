935e37269fe1333fece9cc61525bd5511e43e28d
Vladimir Migashko <migashko@gmail.com>
2016-03-31 11:54:53 +0300

    *boost filesystem

---
+++ wfc/boost.hpp
@@ -0,0 +1,19 @@
+//
+// Author: Vladimir Migashko <migashko@gmail.com>, (C) 2013-2015
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+
+#pragma once
+
+#include <iow/boost.hpp>
+
+namespace wfc{ 
+
+namespace boost{
+
+using namespace ::wfc::boost;
+
+}
+
+}
