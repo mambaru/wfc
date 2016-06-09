70bcf6b2cf133fe5eaaad119900972ff6ac6a052
Vladimir Migashko <migashko@gmail.com>
2016-06-09 20:12:11 +0300

    upd

--- cmake/BuildInfo.cmake
+++ cmake/BuildInfo.cmake
@@ -1,6 +1,7 @@
 GET_FILENAME_COMPONENT(CURRENT_SCRIPT_DIRECTORY ${CMAKE_CURRENT_LIST_FILE} PATH)
 
 MACRO(build_info target_name display_name)
+
   set(build_dir ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}Info)
   set(build_prefix ${build_dir}/${display_name}_build_info)
   string(TOUPPER "CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}" cxx_build_type_flags_name)
@@ -18,9 +19,12 @@ MACRO(build_info target_name display_name)
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     VERBATIM
   )
+
   add_dependencies(${target_name} ${display_name}_build_info)
-target_link_libraries(${target_name} ${build_prefix}.a)
+  target_link_libraries(${target_name} ${build_prefix}.a)
+
   set(clean_list "${CMAKE_CURRENT_SOURCE_DIR}/${display_name}_build_info.h;${build_prefix}1.c;${build_prefix}2.c;${build_prefix}1.c~1;${build_prefix}.a;${build_prefix}1.o;${build_prefix}2.o;")
   SET_DIRECTORY_PROPERTIES(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${clean_list}" )
+  
 ENDMACRO(build_info)
 
