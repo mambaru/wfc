440fb401aaab9b51f62413bed0f3a35e6ac0dbc3
Vladimir Migashko <migashko@gmail.com>
2016-11-21 04:25:22 +0300

    upd

--- wfc/stat/istat.hpp
+++ wfc/stat/istat.hpp
@@ -2,6 +2,7 @@
 
 #include <wfc/iinterface.hpp>
 #include <wrtstat/time_meter.hpp>
+#include <wrtstat/aggregated_data.hpp>
 #include <string>
 
 namespace wfc{
@@ -11,6 +12,9 @@ struct istat: iinterface
   typedef std::chrono::nanoseconds duration_type;
   typedef ::wrtstat::time_meter< duration_type > meter_type;
   typedef std::shared_ptr<meter_type> meter_ptr;
+  typedef ::wrtstat::aggregated_data aggregated_type;
+  typedef std::unique_ptr<aggregated_type> aggregated_ptr;
+
 
   virtual ~istat(){}
 
