1adb0a4869cd0698afbd6e6d19e0ccc1a76173ea
Vladimir Migashko <migashko@gmail.com>
2016-12-08 02:35:05 +0300

    *stat opt

--- wfc/statistics/stat_options.hpp
+++ wfc/statistics/stat_options.hpp
@@ -8,11 +8,6 @@ namespace wfc{
 
 struct stat_options: public ::wrtstat::wrtstat_options
 {
-  /*
-  std::string time_suffix;
-  std::string size_suffix;
-  std::vector<std::string> prefixes;
-  */
 };
 
 }
--- wfc/statistics/stat_options_json.hpp
+++ wfc/statistics/stat_options_json.hpp
@@ -11,15 +11,17 @@ struct stat_options_json
   JSON_NAME(step_ms)
   JSON_NAME(limit)
   JSON_NAME(levels)
+  JSON_NAME(pool)
   JSON_NAME(prefixes)
 
   typedef json::object<
     stat_options,
     json::member_list<
-      json::member< n_reduced_size,  ::wrtstat::aggregator_options, ::wrtstat::types::size_type, & ::wrtstat::aggregator_options::reduced_size>,
+      json::member< n_reduced_size,  ::wrtstat::aggregator_options, ::wrtstat::types::size_type, &::wrtstat::aggregator_options::reduced_size>,
       json::member< n_step_ms, ::wrtstat::separator_options, ::wrtstat::types::time_type, &::wrtstat::separator_options::step_ts>,
       json::member< n_limit, ::wrtstat::reducer_options, ::wrtstat::types::size_type, &::wrtstat::reducer_options::limit>,
       json::member< n_levels, ::wrtstat::reducer_options, ::wrtstat::types::size_type, &::wrtstat::reducer_options::levels>,
+      json::member< n_pool, ::wrtstat::manager_options, size_t, &::wrtstat::manager_options::pool>,
       json::member< n_prefixes, ::wrtstat::wrtstat_options, std::vector<std::string>, &::wrtstat::wrtstat_options::prefixes, json::vector_of_strings<> >
      >
   > type;
