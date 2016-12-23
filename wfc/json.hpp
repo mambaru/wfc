e376d1531f5557c07e3d0529cbb4394b576716a7
Vladimir Migashko <migashko@gmail.com>
2016-12-23 19:51:48 +0300

    build error

--- wfc/json.hpp
+++ wfc/json.hpp
@@ -16,3 +16,19 @@ namespace wfc{ namespace json{
 using namespace wjson;
 
 }}
+
+/*
+std::string operator "" _json(const char* str, size_t size)
+{
+  std::string ss( str, str + size);
+  std::replace( ss.begin(), ss.end(), '\'', '\"');
+  return ss;
+}
+*/
+
+/*
+int main()
+{
+  std::cout << "{'method':'plus','params':{'first':2,'second':3},'id':1}"_json << std::endl;
+}
+*/
