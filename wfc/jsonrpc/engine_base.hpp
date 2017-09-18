4259239e39032b40097cde670d4752f7dcb5d069
Vladimir Migashko <migashko@gmail.com>
2017-09-19 00:42:54 +0300

    -DEBUG getaway-service impl

--- wfc/jsonrpc/engine_base.hpp
+++ wfc/jsonrpc/engine_base.hpp
@@ -60,7 +60,6 @@ public:
   {
     if ( this->suspended() )
       return;
-    DEBUG_LOG_DEBUG( "engine_base::reg_io " << io_id )
     if ( _engine != nullptr )
     {
       _engine->reg_io( io_id, this->wrap([witf]( data_ptr d, io_id_t io_id, output_handler_t handler)
@@ -77,7 +76,6 @@ public:
   {
     if ( this->suspended() )
       return;
-    DEBUG_LOG_DEBUG( "engine_base::unreg_io " << io_id )
     if ( _engine != nullptr )
     {
       _engine->unreg_io(io_id);
@@ -121,7 +119,6 @@ protected:
       _engine->reconfigure( eopt );
     }
 
-    //_workflow = this->get_workflow();
     if ( auto wf = this->get_workflow() )
     {
       wf->release_timer( _timer_id );
@@ -133,7 +130,6 @@ protected:
           [engine]() -> bool 
           {
             JSONRPC_LOG_DEBUG( " JSON-RPC remove outdated...");
-            //JSONRPC_LOG_MESSAGE( "=1= JSON-RPC remove outdated..." );
             if ( size_t count = engine->remove_outdated() )
             {
               JSONRPC_LOG_WARNING( "JSON-RPC: " << count << " calls is outdated.");
@@ -149,17 +145,14 @@ protected:
 private:
   void initialize_statistics_() 
   {
-    //JSONRPC_LOG_MESSAGE( "JSON-RPC: DEBUG STAT -1-");
     if ( auto stat = this->get_statistics() )
     {
       auto sopt = this->statistics_options();
       if ( auto wf = this->get_workflow() )
       {
-        //JSONRPC_LOG_MESSAGE( "JSON-RPC: DEBUG STAT -2-");
         wf->release_timer(_stat_timer_id);
         if ( sopt.interval_ms != 0 )
         {
-          //JSONRPC_LOG_MESSAGE( "JSON-RPC: DEBUG STAT -3-");
           auto name = this->name();
           auto handler_map  = stat->create_value_prototype(name + sopt.handler_map);
           auto result_map   = stat->create_value_prototype(name + sopt.result_map);
@@ -170,7 +163,6 @@ private:
             {
               if ( auto stat = this->get_statistics() )
               {
-                //JSONRPC_LOG_MESSAGE( "JSON-RPC: DEBUG STAT Ready! " << this->name());
                 auto si = this->_engine->sizes();
                 stat->create_meter(handler_map,  si.handler_map, 0);
                 stat->create_meter(result_map,   si.result_map, 0);
@@ -189,7 +181,6 @@ private:
 private:
   timer_id_t _timer_id = 0;
   timer_id_t _stat_timer_id = 0;
-  //std::weak_ptr< workflow_type > _workflow;
 protected:
   engine_ptr _engine;
 };
