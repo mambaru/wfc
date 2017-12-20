b59cbadea5befe38586210d61cbca8d26508f650
Vladimir Migashko <migashko@gmail.com>
2017-12-21 02:04:09 +0300

    *ilogger for wlog

--- wfc/logger/ilogger.hpp
+++ wfc/logger/ilogger.hpp
@@ -2,6 +2,7 @@
 
 #include <wfc/iinterface.hpp>
 #include <wlog/logger_fun.hpp>
+#include <wlog/logger/basic_logger_options.hpp>
 
 #include <string>
 
@@ -10,8 +11,21 @@ namespace wfc{
 struct ilogger: iinterface
 {
   virtual ~ilogger(){}
-  typedef wlog::formatter_fun formatter_t;
-  virtual formatter_t formatter() = 0;
+  typedef wlog::formatter_fun formatter_fun;
+  typedef wlog::writer_fun writer_fun;
+  typedef wlog::basic_logger_options options_type;
+  typedef std::unique_ptr<options_type> options_ptr;
+
+  virtual options_ptr options() { return nullptr; }
+  
+  virtual formatter_fun file_formatter()   { return nullptr;}
+  virtual formatter_fun stdout_formatter() { return nullptr;}
+  virtual formatter_fun syslog_formatter() { return nullptr;}
+  
+  virtual writer_fun file_writer()   { return nullptr;}
+  virtual writer_fun stdout_writer() { return nullptr;}
+  virtual writer_fun syslog_writer() { return nullptr;}
+  
 };
 
 }
