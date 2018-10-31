ae626b74b9620eda7f69a0c08892b3b7e9745678
Vladimir Migashko <migashko@gmail.com>
2018-10-31 19:56:48 +0300

    up

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -4,14 +4,20 @@
 
 MACRO(get_libs liblist)
 
+  message(${liblist})
+  foreach(curlib IN ${liblist})
+    message(${curlib})
+  endforeach(loop_var)
+  return()
   if ( NOT "${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}" )
     return()
   endif()
 
   message(STATUS "${PROJECT_NAME} init submodules")
-  execute_process(COMMAND git submodule update --init WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+  execute_process(COMMAND mc WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
   message(STATUS "${PROJECT_NAME} init submodules DONE")
-  foreach(curlib liblist)
+  message(${liblist})
+  foreach(curlib ${liblist})
     message(curlib)
   endforeach(loop_var)
 
