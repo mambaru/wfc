f2464c5374999dbb0ea7df6c5d87125651181e6e
Vladimir Migashko <migashko@gmail.com>
2018-11-01 01:09:04 +0300

    standlone

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -2,6 +2,12 @@
 # include(cmake/ci.cmake)
 
 if ( CMAKE_CURRENT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR )
+  set(STANDALONE ON)
+else()
+  set(STANDALONE OFF)
+endif()
+
+if ( STANDALONE )
   execute_process(
     COMMAND 
       git submodule update --init -- "external/cmake-ci"
@@ -10,7 +16,6 @@ if ( CMAKE_CURRENT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR )
     RESULT_VARIABLE
       EXIT_CODE
   )
-  
   list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/external/cmake-ci)
   include(getlibs)
 endif()
