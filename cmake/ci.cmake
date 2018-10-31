f666b39805b1048e066c3e3b757b04c4feda0584
Vladimir Migashko <migashko@gmail.com>
2018-10-31 23:48:30 +0300

    *cmake-ci

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -1,24 +1,16 @@
 # copy this file to root project: cp external/cmake-ci/ci.cmake cmake/
 # include(cmake/ci.cmake)
 
-
-MACRO(get_libs liblist)
-
-  message(${liblist})
-  foreach(curlib IN ${liblist})
-    message(${curlib})
-  endforeach(loop_var)
-  return()
-  if ( NOT "${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}" )
-    return()
-  endif()
-
-  message(STATUS "${PROJECT_NAME} init submodules")
-  execute_process(COMMAND mc WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-  message(STATUS "${PROJECT_NAME} init submodules DONE")
-  message(${liblist})
-  foreach(curlib ${liblist})
-    message(curlib)
-  endforeach(loop_var)
-
-ENDMACRO(get_libs)
+if ( CMAKE_CURRENT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR )
+  execute_process(
+    COMMAND 
+      git submodule update --init -- "external/cmake-ci"
+    WORKING_DIRECTORY 
+      ${CMAKE_CURRENT_SOURCE_DIR}
+    RESULT_VARIABLE
+      EXIT_CODE
+  )
+  
+  list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/external/cmake-ci)
+  include(getlibs)
+endif()
